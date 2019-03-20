9cfba6dbfade91dd359710e5789d559f33d6a3f5
dagealuba <1120652730@qq.com>
2019-03-20 17:04:19 +0800

    3-20 login and update userMessage

--- src/pages/home/user.js
+++ src/pages/home/user.js
@@ -84,7 +84,7 @@ class User extends React.Component{
         })
     };
 
-    aftarChangeAvatar = () => {
+    afterChangeAvatar = () => {
         window.location.reload();
     }
 
@@ -133,7 +133,7 @@ class User extends React.Component{
                     onCancel={this.handleCancel}
                     destroyOnClose={"true"}
                     footer={null}
-                    afterClose={this.state.modalContent !== "userPassword" ? this.aftarChangeAvatar : null}
+                    afterClose={this.state.modalContent !== "userPassword" ? this.afterChangeAvatar : null}
                 >
                     {this.state.modalContent === "userMessage" ? userMessage : this.state.modalContent === "avatar" ? avatar : changePassword}
                 </Modal>
